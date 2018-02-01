6ef5a5ff602af7eaf3cf788bd81a292ae2481d91
Noé <alu20468585b@ieselcaminas.org>
2018-02-01 17:37:36 +0100

    prueba

---
+++ Blue JAVA/src/Purebas/prueba1.java
@@ -0,0 +1,10 @@
+package Purebas;
+
+public class prueba1 {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Prueba de envio");
+	}
+
+}
