9b1e5c3a19db7960a2f5d0a19902a47f753c9aaa
Noé <alu20468585b@ieselcaminas.org>
2018-05-25 21:58:30 +0200

    modificaciones en TheSerializer. agregados metodos Objeto/Json Objeto/XML Xml/Json Json/Xml

--- Java_Swing/src/Pruebas/TheSerializer.java
+++ Java_Swing/src/Pruebas/TheSerializer.java
@@ -1,51 +1,54 @@
 package Pruebas;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
+import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
 import org.w3c.dom.Document;
-
-import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
-import java.util.Scanner;
+import java.io.Writer;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
 import java.awt.event.ActionEvent;
-import javax.swing.JTextPane;
-import javax.swing.ScrollPaneConstants;
 import javax.swing.JTextArea;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+
 public class TheSerializer extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2578754006130290751L;
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextArea textArea;
@@ -105,6 +108,11 @@ public class TheSerializer extends JFrame {
 							}else if(f.getCanonicalPath().endsWith(".obj")) {
 								String obj = leerObjetos(f);						
 								textArea.setText(obj);
+							}else if(f.getCanonicalPath().endsWith(".json")) {
+								String json = leerJson(f);
+								textArea.setText(json);
+							}else {
+								JOptionPane.showMessageDialog(textArea, "La extensión del archivo no es valida", "La extensión del archivo no es valida", JOptionPane.ERROR_MESSAGE);
 							}
 						}
 					}catch(ClassNotFoundException | SAXException | ParserConfigurationException cnfe) {
@@ -135,6 +143,30 @@ public class TheSerializer extends JFrame {
 		JButton btnNewButton = new JButton("JSON");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				if((f == null) || (f.getName().equals(""))) {
+					JOptionPane.showMessageDialog(textArea, "No hay un archivo valido seleccionado", "No hay un archivo valido seleccionado", JOptionPane.ERROR_MESSAGE);
+				}else {
+					try {
+						if(getExtension(f).equals(".json")) {
+							JOptionPane.showMessageDialog(textArea, "El archivo seleccionado ya es JSON", "El archivo seleccionado ya es JSON", JOptionPane.ERROR_MESSAGE);
+						}else {
+							File faux = null;
+							if(getExtension(f).equals(".obj")) {
+								faux = ObjetosAJson(f);
+								textArea_1.setText(leerJson(faux));
+								textField.setText(f.getCanonicalPath());
+							}else if(getExtension(f).equals(".xml")) {
+								faux = XmlAJson(f);
+								textArea_1.setText(leerJson(faux));
+								textField.setText(f.getCanonicalPath());
+							}else {
+								JOptionPane.showMessageDialog(textArea, "No se reconoce la extension del archivo", "No se reconoce la extension del archivo", JOptionPane.ERROR_MESSAGE);
+							}
+						}
+					} catch (IOException | ClassNotFoundException | SAXException | ParserConfigurationException e1) {
+						e1.printStackTrace();
+					}
+				}
 			}
 		});
 		btnNewButton.setBounds(0, 0, 89, 23);
@@ -147,11 +179,21 @@ public class TheSerializer extends JFrame {
 					JOptionPane.showMessageDialog(textArea, "No hay un archivo valido seleccionado", "No hay un archivo valido seleccionado", JOptionPane.ERROR_MESSAGE);
 				}else {
 					try {
-						if(f.getCanonicalPath().endsWith(".xml")) {
+						if(getExtension(f).equals(".xml")) {
 							JOptionPane.showMessageDialog(textArea, "El archivo seleccionado ya es XML", "El archivo seleccionado ya es XML", JOptionPane.ERROR_MESSAGE);
 						}else {
-							File f2 = convertirAXml(f);
-							textArea_1.setText(leerXml(f2));
+							File faux2 = null;
+							if(getExtension(f).equals(".obj")) {
+								faux2 = ObjetosAXml(f);
+								textArea_1.setText(leerXml(faux2));
+								textField.setText(f.getCanonicalPath());
+							}else if(getExtension(f).equals(".json")) {
+								faux2 = JsonAXml(f);
+								textArea_1.setText(leerXml(faux2));
+								textField.setText(f.getCanonicalPath());
+							}else {
+								JOptionPane.showMessageDialog(textArea, "No se reconoce la extension del archivo", "No se reconoce la extension del archivo", JOptionPane.ERROR_MESSAGE);
+							}	
 						}
 					} catch (IOException | ClassNotFoundException | ParserConfigurationException | TransformerFactoryConfigurationError | TransformerException | SAXException e1) {
 						e1.printStackTrace();
@@ -174,6 +216,10 @@ public class TheSerializer extends JFrame {
 		scroll_A1.setBounds(10, 239, 564, 161);
 		contentPane.add(scroll_A1);
 	}
+	public static String getExtension(File f) throws IOException {
+		String ext = f.getCanonicalPath();				
+		return ext.substring(ext.lastIndexOf("."), ext.length());
+	}	
 	public static String leerObjetos(File f) throws FileNotFoundException, IOException, ClassNotFoundException {
 		String objs ="";
 		ObjectInputStream f_in = new ObjectInputStream(new FileInputStream(f));
@@ -208,7 +254,18 @@ public class TheSerializer extends JFrame {
 		xmls += "<" + root.getNodeName()+ ">";
 		return xmls;
 	}
-	public static File convertirAXml(File f) throws FileNotFoundException, IOException, ClassNotFoundException, ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException {
+	public static String leerJson(File f) throws FileNotFoundException {
+		String jsons = "";
+		java.io.FileInputStream j_in = new FileInputStream(f);
+		final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(j_in));
+		final Gson gson = new Gson();
+		final java.lang.reflect.Type tipoLista = new TypeToken<List<Empleado>>(){}.getType();
+		final List<Empleado> empleados = gson.fromJson(bufferedReader, tipoLista);
+		final String represerntacionJSON = gson.toJson(empleados);
+		jsons = represerntacionJSON;
+		return jsons;
+	}
+	public static File ObjetosAXml(File f) throws FileNotFoundException, IOException, ClassNotFoundException, ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException {
 		File xml = null;
 		ObjectInputStream f_in = new ObjectInputStream(new FileInputStream(f));
 		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
@@ -246,5 +303,108 @@ public class TheSerializer extends JFrame {
 		trans.transform(source, result);
 		xml = new File("archivos/Empleados_o_x.xml");
 		return xml;
+	}
+	public static File ObjetosAJson(File f) throws FileNotFoundException, IOException, ClassNotFoundException{
+		File jsons = null;
+		ObjectInputStream f_in = new ObjectInputStream(new FileInputStream(f));
+		String jsonEmpleados = "";
+		
+		Empleado e;
+		
+		try {
+			while(true) {
+				e = (Empleado) f_in.readObject();
+				
+				jsonEmpleados += "{" + "\"nombre\":\""  + e.getNombre() + "\","
+									+ "\"telf\":\""  + String.valueOf(e.getTelf()) + "\","
+									+ "\"sueldo\":\"" + String.valueOf(e.getSueldo()) + "\""
+									+ "},";
+			}
+		}catch(EOFException eof) {
+			f_in.close();
+		}
+		
+		jsonEmpleados = jsonEmpleados.substring(0, jsonEmpleados.length()-1);
+		jsonEmpleados = "[" + jsonEmpleados + "]";
+		//System.out.println(jsonEmpleados);
+		final Gson gson = new Gson();
+		final java.lang.reflect.Type tipoLista = new TypeToken<List<Empleado>>(){}.getType();
+		final List<Empleado> posts = gson.fromJson(jsonEmpleados, tipoLista);
+		final Writer w_json = new FileWriter("./archivos/Empleados_o_j.json");		
+		final String representacionJSON = gson.toJson(posts);		
+		w_json.write(representacionJSON);		
+		w_json.close();
+		jsons = new File("./archivos/Empleados_o_j.json");
+		return jsons;
+	}
+	public static File XmlAJson(File f) throws SAXException, IOException, ParserConfigurationException {
+		File xml = null;
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("./archivos/Empleados_o_x.xml");
+		
+		final Writer f_out = new FileWriter("./archivos/Empleados_x_j.json");
+		
+		String jsonEmpleados = "";
+		
+		Element root = (Element) doc.getChildNodes().item(0);
+		NodeList empleados = root.getElementsByTagName("empleado");
+		
+		for(int i = 0; i < empleados.getLength(); i++) {
+			Element empleado = (Element) empleados.item(i);
+			
+			jsonEmpleados += "{" + "\"nombre\":\"" + empleado.getElementsByTagName("nombre").item(0).getFirstChild().getNodeValue() + "\"," 
+								+ "\"telf\":" +  Long.parseLong(empleado.getElementsByTagName("telf").item(0).getFirstChild().getNodeValue()) + ","
+								+ "\"sueldo\":" + Double.parseDouble(empleado.getElementsByTagName("sueldo").item(0).getFirstChild().getNodeValue()) + "},";
+		}
+		
+		jsonEmpleados = jsonEmpleados.substring(0,jsonEmpleados.length()-1);
+		jsonEmpleados = "[" + jsonEmpleados + "]";
+		
+		final Gson gson = new Gson();
+		final java.lang.reflect.Type tipoLista = new TypeToken<List<Empleado>>(){}.getType();
+		final List<Empleado> emp = gson.fromJson(jsonEmpleados, tipoLista);
+		final String JSON = gson.toJson(emp);
+		f_out.write(JSON);
+		f_out.close();
+		xml = new File("./archivos/Empleados_x_j.json");
+		return xml;
+	}
+	public static File JsonAXml (File f) throws ParserConfigurationException, FileNotFoundException, TransformerFactoryConfigurationError, TransformerException {
+		File json = null;
+		FileInputStream f_in = new FileInputStream("./archivos/Empleados_o_j.json");
+		final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(f_in));
+
+		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+		
+		Element root = doc.createElement("empleados");
+		doc.appendChild(root);
+		
+		final Gson gson = new Gson();
+		final java.lang.reflect.Type tipoLista = new TypeToken<List<Empleado>>(){}.getType();
+		final List<Empleado> empleados = gson.fromJson(bufferedReader, tipoLista);		
+		
+		for(Empleado e: empleados) {
+			
+			Element empleado = doc.createElement("empleado");
+			
+			Element nombre = doc.createElement("nombre");
+			nombre.appendChild(doc.createTextNode(e.getNombre()));
+			empleado.appendChild(nombre);
+			
+			Element telf = doc.createElement("telf");
+			telf.appendChild(doc.createTextNode(String.valueOf(e.getTelf())));
+			empleado.appendChild(telf);
+			
+			Element sueldo = doc.createElement("sueldo");
+			sueldo.appendChild(doc.createTextNode(String.valueOf(e.getSueldo())));
+			empleado.appendChild(sueldo);
+			
+			root.appendChild(empleado);			
+		}
+		Transformer trans = TransformerFactory.newInstance().newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(new FileOutputStream("archivos/Empleados_j_x.xml"));
+		trans.transform(source, result);
+		json = new File("archivos/Empleados_j_x.xml");
+		return json;
 	}
 }
